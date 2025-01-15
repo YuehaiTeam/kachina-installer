f9f428c13cf7c5cf69af94ef8e670ae80e6f7e8e
xiaoyu <7547189+xytoki@users.noreply.github.com>
2025-01-15 22:04:29 +0800

    ✨ feat: embed hdiffz

--- src-tauri/src/builder/gen.rs
+++ src-tauri/src/builder/gen.rs
@@ -1,9 +1,11 @@
 use std::path::Path;
 
 use async_compression::tokio::bufread::ZstdEncoder;
+use hdiff_sys::safe_create_single_patch;
+use tokio::io::AsyncWriteExt;
 
 use crate::{
-    cli::arg::GenArgs,
+    cli::GenArgs,
     metadata::{deep_generate_metadata, PatchInfo, PatchItem, RepoMetadata},
     utils::hash::run_hash,
 };
@@ -52,41 +54,36 @@ pub async fn gen_cli(args: GenArgs) {
     // check diffs
     if let Some(diff_vers) = args.diff_vers {
         if !diff_vers.is_empty() {
-            // diff needed, check hdiffz availability
-            let hdiffz_default = {
-                #[cfg(target_os = "windows")]
-                {
-                    "hdiffz.exe"
+            let mut ignore = ignore::gitignore::GitignoreBuilder::new("/");
+            if let Some(diff_ignore) = args.diff_ignore {
+                for ignore_file in diff_ignore.iter() {
+                    ignore.add_line(None, ignore_file).unwrap();
                 }
-                #[cfg(not(target_os = "windows"))]
-                {
-                    "hdiffz"
-                }
-            };
-            let hdiffz = args.hdiffz.unwrap_or(hdiffz_default.to_string());
-            // exec --help to check if hdiffz is available
-            let status = tokio::process::Command::new(&hdiffz)
-                .arg("--help")
-                .status()
-                .await;
-            if status.is_err() {
-                panic!("Diff specified but hdiffz not found ar {:?}", hdiffz);
             }
+            let ignore = ignore.build().unwrap();
             let mut diffs = Vec::new();
             // loop through diff_versions
             for diff_ver in diff_vers.iter() {
                 println!("Generating diff for {}", diff_ver);
                 // loop through current metadata
                 for file in metadata.iter() {
-                    println!("Checking diff for {:?}", file.file_name);
+                    if ignore
+                        .matched_path_or_any_parents(&file.file_name, false)
+                        .is_ignore()
+                    {
+                        println!("File {:?} ignored", file.file_name);
+                        continue;
+                    }
                     // file should > 1M
                     if file.size < 1024 * 1024 {
+                        println!("File {:?} too small, skipped", file.file_name);
                         continue;
                     }
                     // check if file exists in diff_ver
                     let diff_file = Path::new(diff_ver).join(&file.file_name);
                     if !diff_file.exists() {
                         // file not found in diff_ver, skip
+                        println!("File {:?} not found in diff_ver, skipped", file.file_name);
                         continue;
                     }
                     // file found, hash it
@@ -95,34 +92,59 @@ pub async fn gen_cli(args: GenArgs) {
                         .expect("failed to hash diff file");
                     if old_hash == *file.xxh.as_ref().unwrap() {
                         // hash same, skip
+                        println!("File {:?} hash same, skipped", file.file_name);
                         continue;
                     }
                     // hash different, generate diff
                     let output_path = args.output_dir.join(format!(
+                        "{}_{}.hdiff",
+                        old_hash,
+                        file.xxh.as_ref().unwrap()
+                    ));
+                    let compressed_path = args.output_dir.join(format!(
                         "{}_{}",
                         old_hash,
                         file.xxh.as_ref().unwrap()
                     ));
                     println!("Generating diff for {:?} to {:?}", diff_file, output_path);
-                    let status = tokio::process::Command::new(&hdiffz)
-                        .arg("-m-0")
-                        .arg("-SD")
-                        .arg(diff_file.clone())
-                        .arg(args.input_dir.join(&file.file_name))
-                        .arg(output_path.clone())
-                        .stdout(std::process::Stdio::inherit())
-                        .stderr(std::process::Stdio::inherit())
-                        .stdin(std::process::Stdio::inherit())
-                        .status()
-                        .await;
-                    if status.is_err() {
-                        panic!("Failed to generate diff: {:?}", status.err());
-                    }
-                    let status = status.unwrap();
-                    if !status.success() {
-                        panic!("Failed to generate diff: {:?}", status);
-                    }
-                    let diff_size = tokio::fs::metadata(&output_path)
+                    // read old_data and new_data to memory
+                    let old_data = tokio::fs::read(&diff_file)
+                        .await
+                        .expect("failed to read old data");
+                    let new_data = tokio::fs::read(args.input_dir.join(&file.file_name))
+                        .await
+                        .expect("failed to read new data");
+                    let output_file =
+                        std::fs::File::create(&output_path).expect("failed to create output file");
+                    tokio::task::spawn_blocking(move || {
+                        // create output file
+                        safe_create_single_patch(&new_data, &old_data, output_file, 7)
+                    })
+                    .await
+                    .expect("failed to create diff")
+                    .expect("failed to create diff");
+                    // compress diff file
+                    let reader = tokio::fs::File::open(&output_path)
+                        .await
+                        .expect("failed to open diff file");
+                    let reader = tokio::io::BufReader::new(reader);
+                    let mut encoder = ZstdEncoder::new(reader);
+                    let mut writer = tokio::fs::File::create(&compressed_path)
+                        .await
+                        .expect("failed to create compressed diff file");
+                    tokio::io::copy(&mut encoder, &mut writer)
+                        .await
+                        .expect("failed to compress diff");
+                    // flush writer
+                    writer.flush().await.expect("failed to flush writer");
+                    // close file
+                    drop(writer);
+                    // delete uncompressed diff
+                    tokio::fs::remove_file(&output_path)
+                        .await
+                        .expect("failed to remove uncompressed diff");
+                    // if diff size is 50%+ of new file size, delete diff and skip
+                    let diff_size = tokio::fs::metadata(&compressed_path)
                         .await
                         .expect("failed to get diff size")
                         .len();
@@ -130,9 +152,8 @@ pub async fn gen_cli(args: GenArgs) {
                         .await
                         .expect("failed to get old size")
                         .len();
-                    // if diff size is 50%+ of new file size, delete diff and skip
                     if diff_size > (file.size / 2) {
-                        tokio::fs::remove_file(&output_path)
+                        tokio::fs::remove_file(&compressed_path)
                             .await
                             .expect("failed to remove diff");
                         println!("File {:?} diff too large, skipped", file.file_name);
--- src-tauri/src/cli/arg.rs
+++ src-tauri/src/cli/arg.rs
@@ -17,37 +17,6 @@ pub struct InstallArgs {
 }
 
 #[derive(Debug, Clone, clap::Args)]
-pub struct PackArgs {
-    #[clap(long, short = 'o', default_value = "output.exe")]
-    pub output: PathBuf,
-    #[clap(long, short = 'c', default_value = ".config.json")]
-    pub config: PathBuf,
-    #[clap(long, short = 't')]
-    pub image: Option<PathBuf>,
-    #[clap(long, short = 'm')]
-    pub metadata: Option<PathBuf>,
-    #[clap(long, short = 'd')]
-    pub data_dir: Option<PathBuf>,
-}
-
-#[derive(Debug, Clone, clap::Args)]
-pub struct GenArgs {
-    #[clap(long, short = 'i')]
-    pub input_dir: PathBuf,
-    #[clap(long, short = 'm')]
-    pub output_metadata: PathBuf,
-    #[clap(long, short = 'o')]
-    pub output_dir: PathBuf,
-    #[clap(long, short = 'r')]
-    pub repo: String,
-    #[clap(long, short = 't')]
-    pub tag: String,
-    #[clap(long, short = 'd')]
-    pub diff_vers: Option<Vec<String>>,
-    #[clap(long, short = 'z')]
-    pub hdiffz: Option<String>,
-}
-#[derive(Debug, Clone, clap::Args)]
 pub struct UacArgs {
     pub pipe_id: String,
 }
