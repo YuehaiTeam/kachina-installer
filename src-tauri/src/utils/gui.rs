b6aef60e13431d5825458608eace80984c2f799b
xiaoyu <7547189+xytoki@users.noreply.github.com>
2025-03-09 00:22:41 +0800

    🌈 style: make clippy happy

--- src-tauri/src/utils/gui.rs
+++ src-tauri/src/utils/gui.rs
@@ -5,5 +5,5 @@ pub fn is_dark_mode() -> windows_registry::Result<bool> {
     let hkcu = windows_registry::CURRENT_USER;
     let subkey = hkcu.options().read().open(SUBKEY)?;
     let dword: u32 = subkey.get_u32(VALUE)?;
-    Ok((dword == 0).into())
+    Ok(dword == 0)
 }
